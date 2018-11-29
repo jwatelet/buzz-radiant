3d8f5f1e91ddd642662ce8e946b1e1421c54ddd7
Johan Watelet <johanwatelet@gmail.com>
2018-11-29 20:48:27 +0100

    Add clever cloud config

--- src/main/scala/be/jwa/Config.scala
+++ src/main/scala/be/jwa/Config.scala
@@ -6,7 +6,7 @@ case class Config(config: com.typesafe.config.Config) {
   val twitterConsumerKey: String = Option(System.getenv("TWITTER_CONSUMER_KEY")).getOrElse(config.getString("twitter.consumerKey"))
   val twitterConsumerSecret: String = Option(System.getenv("TWITTER_CONSUMER_SECRET")).getOrElse(config.getString("twitter.consumerSecret"))
   val twitterToken: String = Option(System.getenv("TWITTER_TOKEN")).getOrElse(config.getString("twitter.token"))
-  val twitterSecret: String = Option(System.getenv("SECRET")).getOrElse(config.getString("twitter.secret"))
+  val twitterSecret: String = Option(System.getenv("TWITTER_SECRET")).getOrElse(config.getString("twitter.secret"))
   val twitterConfig: (String, String, String, String) = (twitterConsumerKey, twitterConsumerSecret, twitterToken, twitterSecret)
 }
 
