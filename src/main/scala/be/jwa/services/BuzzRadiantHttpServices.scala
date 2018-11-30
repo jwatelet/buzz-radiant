cfb85de4a9251147b4c54ed298bb1fdbbb3222a5
Johan Watelet <johanwatelet@gmail.com>
2018-11-30 11:38:43 +0100

    add / route hello world

--- src/main/scala/be/jwa/services/BuzzRadiantHttpServices.scala
+++ src/main/scala/be/jwa/services/BuzzRadiantHttpServices.scala
@@ -10,10 +10,17 @@ import scala.concurrent.ExecutionContextExecutor
 class BuzzRadiantHttpServices(implicit val twitterActor: ActorRef, implicit val system: ActorSystem, implicit val timeout: Timeout) extends TwitterService with TwitterUserService {
 
   implicit val dispatcher: ExecutionContextExecutor = system.dispatcher
+  val mainRoutes: Route =  pathEndOrSingleSlash {
+    get {
+      complete{
+        "hello world"
+      }
+    }
+  }
 
   val routes: Route = {
     Route.seal(
-      tweetRoutes ~ twitterUserRoutes
+      tweetRoutes ~ twitterUserRoutes ~ mainRoutes
     )
   }
 }
