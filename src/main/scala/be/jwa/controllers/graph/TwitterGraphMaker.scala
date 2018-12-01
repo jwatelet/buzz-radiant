2cd66b3f0dc8a4c164937f615620c227b9691293
Johan Watelet <johanwatelet@gmail.com>
2018-12-01 20:53:47 +0100

    remove sink println and add logging

--- src/main/scala/be/jwa/controllers/graph/TwitterGraphMaker.scala
+++ src/main/scala/be/jwa/controllers/graph/TwitterGraphMaker.scala
@@ -1,27 +1,30 @@
 package be.jwa.controllers.graph
 
-import akka.pattern.ask
 import akka.NotUsed
 import akka.actor.ActorRef
+import akka.pattern.ask
 import akka.stream.ClosedShape
 import akka.stream.scaladsl.{Flow, GraphDSL, RunnableGraph, Sink, Source}
 import akka.util.Timeout
 import be.jwa.actors.TwitterActor.AddTweet
 import be.jwa.controllers.Tweet
+import org.slf4j.LoggerFactory
 
 trait TwitterGraphMaker {
   implicit val timeout: Timeout
+
+  private val logger = LoggerFactory.getLogger(getClass.getName)
 
   def makeTwitterGraph(twitterSource: Source[Tweet, NotUsed], tweetActor: ActorRef): RunnableGraph[NotUsed] = {
     RunnableGraph.fromGraph(GraphDSL.create() { implicit builder =>
       import akka.stream.scaladsl.GraphDSL.Implicits._
 
-      val printlnSink = Sink.foreach(println)
       val flow = Flow[Tweet].map { t =>
         tweetActor ? AddTweet(t)
+        logger.info(t.toString)
         t
       }
-      twitterSource ~> flow ~> printlnSink
+      twitterSource ~> flow ~> Sink.ignore
 
       ClosedShape
     })
