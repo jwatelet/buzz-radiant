6ef289b1cea8b1389a7d08f6bae5a7b5d852250f
Johan Watelet <johanwatelet@gmail.com>
2018-12-03 22:22:55 +0100

    add tests for buzz actor

--- src/test/scala/BuzzActorTest.scala
+++ src/test/scala/BuzzActorTest.scala
@@ -1,11 +1,11 @@
 import java.util.UUID
 
-import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.ActorSystem
 import akka.pattern.ask
 import akka.stream.ActorMaterializer
 import akka.testkit.{ImplicitSender, TestKit}
 import akka.util.Timeout
-import be.jwa.actors.BuzzActor.{CreateBuzzObserver, GetAllBuzzObserversIds, SendMessageToTwitterActor}
+import be.jwa.actors.BuzzActor.{CreateBuzzObserver, DeleteBuzzObserver, GetAllBuzzObserversIds, SendMessageToTwitterActor}
 import be.jwa.actors.TwitterActor.{GetTweetCount, GetTweets, GetUsersCount}
 import be.jwa.actors.{BuzzActor, TweetCount, TwitterUserCount}
 import be.jwa.controllers.{Tweet, TwitterUser}
@@ -24,62 +24,66 @@ class BuzzActorTest() extends TestKit(ActorSystem("buzz-actor-test")) with Impli
 
   "An Buzz Actor " must {
 
+    val buzzActor = system.actorOf(BuzzActor.props())
     "create a buzz observer" in {
-
-      val buzzActor = system.actorOf(BuzzActor.props())
       buzzActor ! CreateBuzzObserver(Seq("#buzz", "#test"))
-
       expectMsgType[UUID](5.seconds)
     }
-  }
-
-  "get all buzz observers, the right number you have instanciate" in {
-    val buzzActor = system.actorOf(BuzzActor.props())
-
-    Await.result(buzzActor ? CreateBuzzObserver(Seq("#buzz", "#test")), 5.seconds)
-    Await.result(buzzActor ? CreateBuzzObserver(Seq("#buzz2", "#test2")), 5.seconds)
-
-    Await.result((buzzActor ? GetAllBuzzObserversIds).mapTo[Map[UUID, ActorRef]], 5.seconds).size shouldEqual 2
-  }
 
 
-  "instanciate a buzz observer and send tweet count message" in {
-    val buzzActor = system.actorOf(BuzzActor.props())
+    "get all buzz observers, the right number you have instanciate" in {
 
-    val id: UUID = Await.result((buzzActor ? CreateBuzzObserver(Seq("#buzz", "#test"))).mapTo[UUID], 5.seconds)
+      Await.result(buzzActor ? CreateBuzzObserver(Seq("#buzz", "#test")), 5.seconds)
+      Await.result(buzzActor ? CreateBuzzObserver(Seq("#buzz2", "#test2")), 5.seconds)
 
-    buzzActor ! SendMessageToTwitterActor(id, GetTweetCount)
+      Await.result((buzzActor ? GetAllBuzzObserversIds).mapTo[Set[UUID]], 5.seconds).size shouldEqual 3
+    }
 
-    expectMsgType[Option[TweetCount]]
-  }
 
-  "instanciate a buzz observer and send users count message" in {
-    val buzzActor = system.actorOf(BuzzActor.props())
+    "instanciate a buzz observer and send tweet count message" in {
 
-    val id: UUID = Await.result((buzzActor ? CreateBuzzObserver(Seq("#buzz", "#test"))).mapTo[UUID], 5.seconds)
+      val ids: Set[UUID] = Await.result((buzzActor ? GetAllBuzzObserversIds).mapTo[Set[UUID]], 5.seconds)
 
-    buzzActor ! SendMessageToTwitterActor(id, GetUsersCount)
+      ids.foreach(id => buzzActor ! SendMessageToTwitterActor(id, GetTweetCount))
 
-    expectMsgType[Option[TwitterUserCount]]
-  }
+      expectMsgType[Option[TweetCount]]
+    }
 
-  "instanciate a buzz observer and send get all tweet message" in {
-    val buzzActor = system.actorOf(BuzzActor.props())
+    "instanciate a buzz observer and send users count message" in {
 
-    val id: UUID = Await.result((buzzActor ? CreateBuzzObserver(Seq("#buzz", "#test"))).mapTo[UUID], 5.seconds)
+      val ids: Set[UUID] = Await.result((buzzActor ? GetAllBuzzObserversIds).mapTo[Set[UUID]], 5.seconds)
 
-    buzzActor ! SendMessageToTwitterActor(id, GetTweets)
+      ids.foreach(id => buzzActor ! SendMessageToTwitterActor(id, GetUsersCount))
 
-    expectMsgType[Option[Seq[Tweet]]]
-  }
+      expectMsgType[Option[TwitterUserCount]]
+    }
 
-  "instanciate a buzz observer and send get all users message" in {
-    val buzzActor = system.actorOf(BuzzActor.props())
+    "instanciate a buzz observer and send get all tweet message" in {
 
-    val id: UUID = Await.result((buzzActor ? CreateBuzzObserver(Seq("#buzz", "#test"))).mapTo[UUID], 5.seconds)
+      val ids: Set[UUID] = Await.result((buzzActor ? GetAllBuzzObserversIds).mapTo[Set[UUID]], 5.seconds)
 
-    buzzActor ! SendMessageToTwitterActor(id, GetTweets)
+      ids.foreach(id => buzzActor ! SendMessageToTwitterActor(id, GetTweets))
 
-    expectMsgType[Option[Seq[TwitterUser]]]
+      expectMsgType[Option[Seq[Tweet]]]
+    }
+
+    "instanciate a buzz observer and send get all users message" in {
+
+      val ids: Set[UUID] = Await.result((buzzActor ? GetAllBuzzObserversIds).mapTo[Set[UUID]], 5.seconds)
+
+      ids.foreach(id =>  buzzActor ! SendMessageToTwitterActor(id, GetTweets))
+
+      expectMsgType[Option[Seq[TwitterUser]]]
+    }
+
+    "Delete all observers" in {
+
+      val observersIds: Set[UUID] = Await.result((buzzActor ? GetAllBuzzObserversIds).mapTo[Set[UUID]], 5.seconds)
+
+      observersIds.foreach(id => buzzActor ! DeleteBuzzObserver(id))
+
+      Await.result((buzzActor ? GetAllBuzzObserversIds).mapTo[Set[UUID]], 5.seconds).size shouldEqual 0
+    }
+
   }
 }
